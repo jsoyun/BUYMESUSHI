d87827e2c9b95c1a147eb68185c4a16759c449a2
Rina <fghjiff8@gmail.com>
2021-11-15 11:36:12 +0900

    minor change

--- client/src/components/views/AboutPage/About.js
+++ client/src/components/views/AboutPage/About.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <img src="img/video/trashhhhhh .gif" style={{ width: "100vw" }} />
+      <img src="img/video/trashhhhhh.gif" style={{ width: "100vw" }} alt="" />
     </div>
   );
 };
