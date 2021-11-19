6e21be83fe4a8d1ffa89a035c59603cd44ef1cae
Rina <fghjiff8@gmail.com>
2021-11-19 15:47:27 +0900

    Revert "미니게임 페이지 진행중"
    This reverts commit 7f5823decfb1af547e767aa0ba16dcd21e82547b.

--- client/src/components/views/MinigamePage/GameBoard.js
+++ client/src/components/views/MinigamePage/GameBoard.js
@@ -1,5 +1,14 @@
 import LetterGrid from "./LetterGrid";
 
 export default function GameBoard({ secreteWord }) {
+<<<<<<< HEAD
   return <LetterGrid secreteWord={secreteWord} guessedLetters={("a", "r")} />;
+=======
+  return (
+    <div>
+      <LetterGrid secreteWord={secreteWord} guessedLetters={("a", "r")} />
+      <ButtonGrid />
+    </div>
+  );
+>>>>>>> parent of 7f5823d (미니게임 페이지 진행중)
 }
