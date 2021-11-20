11b406650146780ec7d7476e9fdf4f10bbe33795
Seongjae <robbyl95@hanmail.net>
2021-11-20 17:03:31 +0900

    게시판 끝

--- client/src/components/views/BoardPage/BoardDetail.js
+++ client/src/components/views/BoardPage/BoardDetail.js
@@ -10,80 +10,7 @@ import styled from "styled-components";
 
 // props로 상위 컴포넌트의 게시글 값 받아오기
 function BoardDetail(props) {
-  // const [lastIdx, setLastIdx] = useState(0);
-  // const [Title, setTitle] = useState("");
-  // const [Body, setBody] = useState("");
-  // const [Data, setData] = useState([
-  //   {
-  //     _id: "",
-  //     index: "",
-  //     title: "",
-  //     boardBody: "",
-  //     viewcount: "",
-  //     postedBy: "",
-  //   },
-  // ]);
-  // const user = useSelector((state) => state.user.userData);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const res = await axios.get("/api/board");
-  //       console.log(res.data.Boards);
-
-  //       const _Data = await res.data.Boards.find(
-  //         (rowData) => (
-  //           setLastIdx(lastIdx + 1),
-  //           {
-  //             _id: rowData._id,
-  //             index: lastIdx,
-  //             title: rowData.title,
-  //             boardBody: rowData.boardBody,
-  //             viewcount: rowData.viewcount,
-  //             postedBy: rowData.postedBy.nickname,
-  //           }
-  //         )
-  //       );
-  //       setData(Data.concat(_Data));
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
-  ////////////////////////////////////////////////////////
-  ////////////////////////////////////////////////////////
-  // const [BoardDetail, setBoardDetail] = useState([
-  //   {
-  //     _id: "",
-  //     index: "",
-  //     title: "",
-  //     boardBody: "",
-  //     viewcount: "",
-  //     postedBy: "",
-  //   }
-  // ]);
-
-  // useEffect(() => {
-  //   axios.get(`/api/board`).then((res) => {
-  //     console.log("해당 게시글 불러오기 성공");
-  //     setBoardDetail(res.data);
-  //   });
-  // }, []);
-
-  // axios.get("/api/board/:_id", { Body, Title }).then((res) => {
-  //   console.log(res.data);
-  //   console.log("해당 게시글 불러오기 성공");
-  // });
-  ////////////////////////////////////////////////////////
-  ////////////////////////////////////////////////////////
-  ////////////////////////////////////////////////////////
-  ////////////////////////////////////////////////////////
-
-
-  console.log(props.boardsData[1].title, '보드데이터');
-  console.log(props.clickData, '클릭데이터');
-  // 결국 props.boardsData[props.clickData].title  이 값을 찾으면 됨
+  console.log(props.clickData.detailTitle, '클릭데이터');
 
   return (
 
@@ -105,32 +32,24 @@ function BoardDetail(props) {
 
 
               <tr>
-                <th>번호</th>
-                <td>{props.boardsData[props.clickData].title}</td>
-                <th>조회수</th>
-                <td>{props.boardsData[props.clickData].viewCount}</td>
                 <th>작성자</th>
-                <td>{props.boardsData[props.clickData].postedBy}</td>
+                <td>{props.clickData.detailPostedBy}</td>
+                <th>글번호</th>
+                <td>{props.clickData.detailIndex + 1}</td>
               </tr>
               <tr>
                 <th>제목</th>
-                <td colSpan="5">{props.boardsData[props.clickData].title}</td>
+                <td colSpan="5">{props.clickData.detailTitle}</td>
               </tr>
               <tr>
                 <th>작성일</th>
-                <td colSpan="2">{props.boardsData[props.clickData].createdAt}</td>
-                <th>수정일</th>
-                <td colSpan="2">{props.boardsData[props.clickData].updatedAt}</td>
-              </tr>
-              <tr>
-                <th>이미지</th>
-                <td colSpan="6" height="400" style={{ maxWidth: "600px", maxHeight: "600px" }}>
-                  <img src="{props.boardsData[props.clickData].img}" />
-                </td>
+                {props.clickData.detailCreatedAt}
               </tr>
               <tr>
                 <th>내 용</th>
-                <td colSpan="6" height="400" style={{ textAlign: "left" }} >{props.boardsData[props.clickData].boardBody}</td>
+                <td colSpan="6" height="400" style={{ textAlign: "left" }} >
+                  {props.clickData.detailBody}
+                </td>
               </tr>
 
             </tbody>
@@ -140,12 +59,6 @@ function BoardDetail(props) {
 
         </div>
       </div>
-      {/* <div >
-            {% if comments.UserId === user.id%}
-            <a href="/board\{{comments.id}}/boardedit" style="margin-left: 200px; font-weight: bold;">수정</a>     
-            <a href="/board\{{comments.id}}/delete" style="margin-left: 50px; font-weight: bold;">삭제</a> 
-            {% endif %}
-        </div> */}
 
     </div>
   );
