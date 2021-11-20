fc66d76df34ddc2bb4b8e350281d25c51b4b6942
Rina <fghjiff8@gmail.com>
2021-11-20 16:16:10 +0900

    인증페이지 css 수정중

--- client/src/components/views/AuthBoard/AuthBoard.js
+++ client/src/components/views/AuthBoard/AuthBoard.js
@@ -7,17 +7,34 @@ import AuthBoardFeedsCard from "./Sections/AuthBoardFeedsCard";
 import "./AuthBoard.css";
 import AuthBoardComments from "./Sections/AuthBoardComments";
 
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // maxWidth: "100vw",
+    height: "100%",
+    backgroundImage: `url(${
+      process.env.PUBLIC_URL + "/img/authBoard/bg5.jpg"
+    })`,
+    backgroundRepeat: "repeat",
+    // backgroundSize: "cover",
+  },
+}));
+
 const AuthBoard = (props) => {
-    const dispatch = useDispatch();
-    return (
-        <div className="authBoard-container">
-            <h1>지키미인증 페이지</h1>
-            <React.Fragment>
-                <AuthBoardPost />
-                <AuthBoardFeedsCard />
-            </React.Fragment>
-        </div>
-    );
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  return (
+    <div className={classes.root}>
+      <div className="authBoard-container">
+        <h1>지키미인증 페이지</h1>
+        <React.Fragment>
+          <AuthBoardPost />
+          <AuthBoardFeedsCard />
+        </React.Fragment>
+      </div>
+    </div>
+  );
 };
 
 export default withRouter(AuthBoard);
--- client/src/components/views/AuthBoard/Sections/AuthBoardComments.js
+++ client/src/components/views/AuthBoard/Sections/AuthBoardComments.js
@@ -13,128 +13,128 @@ import Button from "@mui/material/Button";
 const ariaLabel = { "aria-label": "description" };
 
 export default function AuthBoardComments({ authBoards, commentsData }) {
-    const user = useSelector((state) => state.user.userData);
-    const [comments, setComments] = useState([]);
+  const user = useSelector((state) => state.user.userData);
+  const [comments, setComments] = useState([]);
 
-    const onCommentsHandler = (e) => {
-        setComments(e.currentTarget.value);
+  const onCommentsHandler = (e) => {
+    setComments(e.currentTarget.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    if (comments == "") {
+      event.preventDefault();
+      return alert("내용을 입력해 주세요!");
+    }
+
+    let body = {
+      comments: comments,
+      authBoardId: authBoards,
     };
 
-    const onSubmitHandler = (event) => {
-        if (comments == "") {
-            event.preventDefault();
-            return alert("내용을 입력해 주세요!");
-        }
+    axios.post("/api/authBoard/comments", body).then((response) => {
+      console.log("댓글 등록 완료");
+    });
+  };
 
-        let body = {
-            comments: comments,
-            authBoardId: authBoards,
-        };
+  // useEffect(() => {
+  //     const fetchData = async () => {
+  //         try {
+  //             const res = await axios.get("/api/authboard");
+  //             // console.log(res.data.resultAuthBoards);
 
-        axios.post("/api/authBoard/comments", body).then((response) => {
-            console.log("댓글 등록 완료");
-        });
-    };
+  //             const _Data = await res.data.resultAuthBoards.map(
+  //                 (rowData) => ({
+  //                     comment: rowData.comments,
+  //                     // _id: rowData._id,
+  //                     // authBody: rowData.authBody,
+  //                     // photo: rowData.photo,
+  //                     // postedBy: rowData.postedBy.nickname,
+  //                     // likes: rowData.likes,
+  //                     // dislikes: rowData.dislikes,
+  //                 })
+  //             );
+  //         } catch (error) {
+  //             console.error(error);
+  //         }
+  //     };
+  //     fetchData();
+  // }, []);
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         try {
-    //             const res = await axios.get("/api/authboard");
-    //             // console.log(res.data.resultAuthBoards);
-
-    //             const _Data = await res.data.resultAuthBoards.map(
-    //                 (rowData) => ({
-    //                     comment: rowData.comments,
-    //                     // _id: rowData._id,
-    //                     // authBody: rowData.authBody,
-    //                     // photo: rowData.photo,
-    //                     // postedBy: rowData.postedBy.nickname,
-    //                     // likes: rowData.likes,
-    //                     // dislikes: rowData.dislikes,
-    //                 })
-    //             );
-    //         } catch (error) {
-    //             console.error(error);
-    //         }
-    //     };
-    //     fetchData();
-    // }, []);
-
-    return (
-        <React.Fragment>
-            {commentsData.map((rowData, index) => (
-                <List
-                    sx={{
-                        width: "100%",
-                        maxWidth: 600,
-                        bgcolor: "background.paper",
-                    }}
-                    key={index}
-                >
-                    <ListItem alignItems="flex-start">
-                        <ListItemAvatar>
-                            <Avatar
-                                alt="Remy Sharp"
-                                // 댓글 아바타 이미지
-                                src={rowData.postedBy.profileImage}
-                            />
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary={
-                                <React.Fragment>
-                                    <Typography
-                                        sx={{ display: "inline" }}
-                                        component="span"
-                                        variant="body2"
-                                        color="text.primary"
-                                    >
-                                        <a
-                                            href={`/profile/${rowData.postedBy.nickname}`}
-                                            style={{
-                                                textDecoration: "none",
-                                                color: "black",
-                                            }}
-                                        >
-                                            {rowData.postedBy.nickname}
-                                        </a>
-                                    </Typography>
-                                </React.Fragment>
-                            }
-                            secondary={rowData.text}
-                        />
-                    </ListItem>
-                </List>
-            ))}
-            <div className="authboard-commentPost-container">
-                <Avatar alt="Remy Sharp" src={user.profileImage} />
-                <form onSubmit={onSubmitHandler} id="authboard_commentPost">
-                    <Input
-                        type="text"
-                        name="authComment"
-                        value={comments}
-                        onChange={onCommentsHandler}
-                        inputProps={ariaLabel}
-                        sx={{ ml: 2, mt: 2, mr: 2 }}
-                        style={{ width: "430px" }}
-                    />
-                    {/* <input
+  return (
+    <React.Fragment>
+      {commentsData.map((rowData, index) => (
+        <List
+          sx={{
+            width: "100%",
+            maxWidth: 600,
+            bgcolor: "background.paper",
+          }}
+          key={index}
+        >
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar
+                alt="Remy Sharp"
+                // 댓글 아바타 이미지
+                src={rowData.postedBy.profileImage}
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    <a
+                      href={`/profile/${rowData.postedBy.nickname}`}
+                      style={{
+                        textDecoration: "none",
+                        color: "black",
+                      }}
+                    >
+                      {rowData.postedBy.nickname}
+                    </a>
+                  </Typography>
+                </React.Fragment>
+              }
+              secondary={rowData.text}
+            />
+          </ListItem>
+        </List>
+      ))}
+      <div className="authboard-commentPost-container">
+        <Avatar alt="Remy Sharp" src={user.profileImage} />
+        <form onSubmit={onSubmitHandler} id="authboard_commentPost">
+          <Input
+            type="text"
+            name="authComment"
+            value={comments}
+            onChange={onCommentsHandler}
+            inputProps={ariaLabel}
+            sx={{ ml: 2, mt: 2, mr: 2 }}
+            style={{ width: "430px" }}
+          />
+          {/* <input
                         type="text"
                         name="authComment"
                         value={comments}
                         onChange={onCommentsHandler}
                     /> */}
-                    <input
-                        name="authBoardsId"
-                        type="text"
-                        value={authBoards}
-                        hidden
-                        readOnly
-                    />
-                    <Button variant="outlined" type="submit">
-                        게시
-                    </Button>
-                </form>
-            </div>
-        </React.Fragment>
-    );
+          <input
+            name="authBoardsId"
+            type="text"
+            value={authBoards}
+            hidden
+            readOnly
+          />
+          <Button variant="outlined" type="submit">
+            게시
+          </Button>
+        </form>
+      </div>
+    </React.Fragment>
+  );
 }
