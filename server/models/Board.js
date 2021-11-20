ffe2be5a670c40abb814f81a8d53ac72994643a6
Seongjae <robbyl95@hanmail.net>
2021-11-20 14:29:13 +0900

    중간

--- server/models/Board.js
+++ server/models/Board.js
@@ -2,28 +2,61 @@ const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Schema.Types;
 
 const BoardSchema = mongoose.Schema({
-    // index: {type:},
+    seq: { type: Number, default: 0 },
     title: { type: String, required: true },
     boardBody: { type: String, required: true },
     img: { type: String },
-    // comments: [
-    //   {
-    //     text: { type: String },
-    //     postedBy: { type: ObjectId, ref: "User" },
-    //   },
-    // ],
     postedBy: { type: ObjectId, ref: 'User' },
     createdAt: { type: Date, default: Date.now },
-    // updatedAt: { type: Date },
+    updatedAt: { type: Date },
     viewcount: {
         type: Number,
         default: 0,
     },
 });
-
-BoardSchema.pre('save', function (next) {
-    const Board = this;
-});
+BoardSchema
+    // .plugin(
+    //     autoIncrement.plugin,
+    //     {
+    //         model: 'boards',
+    //         field: 'seq',
+    //         startAt: 1, //시작
+    //         increment: 1 // 증가
+    //     })
+    .pre('save', function (next) {
+        const Board = this;
+    });
 
 const Board = mongoose.model('Board', BoardSchema);
 module.exports = Board;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// const mongoose = require('mongoose');
+// const { ObjectId } = mongoose.Schema.Types;
+// const User = require('./User');
+// const autoIncrement = require('mongoose-auto-increment');
+// autoIncrement.initialize(mongoose.connection);
+
+// const BoardSchema = new mongoose.Schema({
+//     title: { type: String, required: true },
+//     boardBody: { type: String, required: true },
+//     img: { type: String },
+//     postedBy: { type: ObjectId, ref: 'User' },
+//     createdAt: { type: Date, default: Date.now },
+//     seq: { type: Number, default: 0 },
+
+// }, { collection: 'BoardSchema', versionKey: false });
+
+// BoardSchema.plugin(
+//     autoIncrement.plugin,
+//     {
+//         model: 'BoardSchema',
+//         field: 'seq',
+//         startAt: 1, //시작
+//         increment: 1 // 증가
+//     });
+
+// mongoose.model('BoardSchema', BoardSchema);
+// const C = mongoose.connection.useDb("boards");
+// module.exports = C.model("BoardSchema", BoardSchema);
